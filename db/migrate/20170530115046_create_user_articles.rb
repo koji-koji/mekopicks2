ee8d25079e24d824e1a59778e9dbf75f91bbb128
koji-koji <k.koyama37@gmail.com>
2017-05-30 21:01:28 +0900

    user_articlesテーブルとモデルの作成

--- db/migrate/20170530115046_create_user_articles.rb
+++ db/migrate/20170530115046_create_user_articles.rb
@@ -1,6 +1,6 @@
-class CreateUsersArticles < ActiveRecord::Migration[5.0]
+class CreateUserArticles < ActiveRecord::Migration[5.0]
   def change
-    create_table :users_articles, id: false do |t|
+    create_table :user_articles, id: false do |t|
       t.references :user, foreign_key: true, null: false
       t.references :article, foreign_key: true, null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530105003) do
+ActiveRecord::Schema.define(version: 20170530115046) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20170530105003) do
     t.string   "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_articles", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id",    null: false
+    t.integer "article_id", null: false
+    t.index ["article_id"], name: "index_user_articles_on_article_id", using: :btree
+    t.index ["user_id"], name: "index_user_articles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -48,13 +55,6 @@ ActiveRecord::Schema.define(version: 20170530105003) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "users_articles", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "user_id",    null: false
-    t.integer "article_id", null: false
-    t.index ["article_id"], name: "index_users_articles_on_article_id", using: :btree
-    t.index ["user_id"], name: "index_users_articles_on_user_id", using: :btree
-  end
-
-  add_foreign_key "users_articles", "articles"
-  add_foreign_key "users_articles", "users"
+  add_foreign_key "user_articles", "articles"
+  add_foreign_key "user_articles", "users"
 end
