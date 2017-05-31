3939c0338bc958e89a1873a31a47c13de82261a4
koji-koji <k.koyama37@gmail.com>
2017-05-31 19:27:58 +0900

    ルーティングの変更
    セッションpathとパスワードpathにアクセスできるように追記した。
    devise_for :usersがあるので、不要なresources :usersを削除した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
   root 'articles#index'
   resources :picks
   resources :articles
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
