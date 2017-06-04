760e1cce89ed45758a5cd11514bba2e7a1f1bc65
koji-koji <k.koyama37@gmail.com>
2017-06-04 20:59:12 +0900

    外部キー設定
    外部キーとon update cascadeを設定した。

--- db/migrate/20170529114235_create_picks.rb
+++ db/migrate/20170529114235_create_picks.rb
@@ -1,8 +1,8 @@
 class CreatePicks < ActiveRecord::Migration[5.0]
   def change
     create_table :picks do |t|
-      t.string :user_id
-      t.string :article_id
+      t.string :user_id, foreign_key: true
+      t.string :article_id, foreign_key: true
       t.text :comment
       t.timestamps
     end
