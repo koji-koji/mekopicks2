725f3b6b6140bb3b2f011c5d51289b659f6ab2f4
koji-koji <k.koyama37@gmail.com>
2017-06-10 14:47:28 +0900

    Revert "デプロイ方法の変更"

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,10 +7,10 @@ app_path = File.expand_path('../../../', __FILE__)
 # stderr_path "#{app_path}/log/unicorn.stderr.log"
 # stdout_path "#{app_path}/log/unicorn.stdout.log"
 
-working_directory "#{app_path}/tmp/pids/unicorn.pid"
+working_directory "#{app_path}/current"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
