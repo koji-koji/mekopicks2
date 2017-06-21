bce4ec231befc5b96acdcfc523b7496e65717f3b
koji-koji <k.koyama37@gmail.com>
2017-06-21 16:15:30 +0900

    インクリメンタルサーチのindexアクション設定

---
+++ app/controllers/tag_search_controller.rb
@@ -0,0 +1,10 @@
+class TagSearchController < ApplicationController
+
+  def index
+    @tags = Tag.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.json
+    end
+  end
+
+end
