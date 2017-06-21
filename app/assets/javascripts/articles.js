f1c3a1ddb0c28819a1cbf379725f4d334c823a0b
koji-koji <k.koyama37@gmail.com>
2017-06-21 18:00:40 +0900

    インクリメンタルサーチタグの表示
    ・インクリメンタルサーチタグの表示

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,19 +1,32 @@
 $(function(){
+  function buildHTML(data) {
+    $.each(data, function(i, tags){
+      $('.tag_search').append(
+        `<li class = "add_tag" data-id="${tags.id}">
+          ${tags.name}
+        </li>
+        `
+      )
+    })
+  }
+
   $('.text_field').on('keyup', function(e){
     e.preventDefault();
     var textField = $('.text_field')
     var input = textField.val()
+    $('.tag_search').empty()
     $.ajax({
       type: 'GET',
       url: '/tag_search',
-      data:('keyword=' + input),
+      data: ('keyword=' + input),
       dataType: 'json'
     })
     .done(function(data){
+      $('.tag_search').append('<li>').append(data.name)
+      buildHTML(data)
     })
     .fail(function(error){
       console.log("hoge")
-
     })
   })
 });
