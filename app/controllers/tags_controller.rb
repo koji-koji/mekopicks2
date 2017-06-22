870635ad12c41172c36af999a07a93dc53208b4b
koji-koji <k.koyama37@gmail.com>
2017-06-22 10:49:51 +0900

    表示される記事のユニーク設定

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,7 +11,7 @@ class TagsController < ApplicationController
   # GET /tags/1.json
   def show
     @tag = Tag.find(params[:id])
-    @articles = @tag.articles
+    @articles = @tag.articles.uniq
   end
 
   # GET /tags/new
