34e0e564f8d6d82842004f8cff535e6a2d26dd01
koji-koji <k.koyama37@gmail.com>
2017-06-26 19:30:43 +0900

    バリデーションの変更
    pick.rbに誤って書いていたため、article.rbに記述を変更した。

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   # mount_uploader :image, ImagesUploader
+  validates :url, presence: true
   has_many :users, through: :user_articles
   has_many :tags, through: :article_tags
   has_many :picks
